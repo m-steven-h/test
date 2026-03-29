17c9e0a5de9403c3ba1951e5d11e546f80abb32f
STEVEN HANY <steven.hany.194@gmail.com>
2026-03-29 02:42:07 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true // 🔥 حل المشكلة
     }
 
     kotlinOptions {
@@ -31,6 +32,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    // 🔥 مهم لتشغيل flutter_local_notifications
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
