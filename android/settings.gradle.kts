fac7db82ee60c643f8cdedca8b1e73ca07a903dc
STEVEN HANY <steven.hany.194@gmail.com>
2026-03-30 14:20:21 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,15 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    def flutterSdkPath = {
+        def plugins = new Properties()
+        file("..\\..\\android\\local.properties").withInputStream { stream ->
+            plugins.load(stream)
         }
+        def flutterSdkPath = plugins.getProperty("flutter.sdk")
+        if (flutterSdkPath == null) {
+            throw new GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
+        }
+        return flutterSdkPath
+    }()
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -18,9 +21,9 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-id("com.android.application") version "8.7.0" apply false
-id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
+    id "com.android.application" version "8.2.0" apply false
+    id "org.jetbrains.kotlin.android" version "2.0.0" apply false  // ✅ Kotlin 2.0.0
 }
 
-include(":app")
+include ":app"
